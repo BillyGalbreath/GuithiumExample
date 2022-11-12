62bfda24956058cf48ca94e8d8b267c6749c9812
BillyGalbreath <blake.galbreath@gmail.com>
2022-11-12 05:49:11 -0600

    even simpler

--- src/main/java/net/pl3x/servergui/test/PlayerListener.java
+++ src/main/java/net/pl3x/servergui/test/PlayerListener.java
@@ -3,36 +3,41 @@ package net.pl3x.servergui.test;
 import net.pl3x.servergui.api.gui.element.Image;
 import net.pl3x.servergui.api.gui.element.Text;
 import net.pl3x.servergui.plugin.event.HelloEvent;
-import net.pl3x.servergui.plugin.network.packet.ElementPacket;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import java.util.UUID;
+
 public class PlayerListener implements Listener {
     @EventHandler
     public void onHello(HelloEvent event) {
         Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
 
         updateCoordsHUD(player, player.getLocation());
 
         // test stuff
-        ElementPacket.send(player, Image.builder("test:hayley")
+        Image.builder("test:hayley")
             .setSize(120, 150)
             .setPos(0, 20)
-            .build());
-        ElementPacket.send(player, Text.builder("test:crosshair")
+            .build()
+            .send(uuid);
+        Text.builder("test:crosshair")
             .setText("O")
             .setAnchor(0.5F, 0.5F)
             .setOffset(0.5F, 0.5F)
             .setShadow(false)
-            .build());
-        ElementPacket.send(player, Text.builder("test:footnote")
+            .build()
+            .send(uuid);
+        Text.builder("test:footnote")
             .setText("bottom right")
             .setAnchor(1, 1)
             .setOffset(1, 1)
-            .build());
+            .build()
+            .send(uuid);
     }
 
     @EventHandler
@@ -43,9 +48,10 @@ public class PlayerListener implements Listener {
     }
 
     private void updateCoordsHUD(Player player, Location loc) {
-        Text coords = Text.builder("test:coords")
-            .setText(loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ())
-            .build();
-        new ElementPacket().send(player, coords);
+        String coords = loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ();
+        Text.builder("test:coords")
+            .setText(coords)
+            .build()
+            .send(player.getUniqueId());
     }
 }
